2f7fe81a0eccec67ce7663133c9795f7f84728ee
Ali isa Tas <alisatas9292@gmail.com>
2024-01-18 20:48:43 +0100

    homework11

---
+++ src/main/java/homework11/DepartmantTest.java
@@ -0,0 +1,15 @@
+package homework11;
+
+public class DepartmantTest extends Departmant {
+    public static void main(String[] args) {
+        Male maleTeamLead = new Male("Ali","Tas","07.05.1992");
+        Female femaleStaff = new Female("ayse","fatma","07.05.1940");
+        Department department = new Department(maleTeamLead);
+        department.addStaff(femaleStaff);
+        department.removeStaff(maleTeamLead);
+        department.addTask("Task 1");
+        System.out.println("Task1 added");
+        department.addTask("Task 2");
+        System.out.println("Task2 added");
+    }
+}
